df856ef462656844f3372f9c7a96f13ea745dbd8
r12esh <ritesh098765432@gmail.com>
2023-03-25 12:12:32 +0530

    add close accordion on click outside feature

--- src/pages/homepage/components/faq/Faq.jsx
+++ src/pages/homepage/components/faq/Faq.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { SlArrowDown, SlArrowUp } from "react-icons/sl";
+import OnClickOutside from "../../../../shared/onClickOutside/OnClickOutside";
 import "./faqsection.css";
 
 export default function Faq({ id, active, setActive, question, answer }) {
@@ -10,33 +11,29 @@ export default function Faq({ id, active, setActive, question, answer }) {
       setActive(id);
     }
   }
-  function logit() {
+  const closeAllAccordion = () => {
+    console.log("Hi htis is cosoefosihefi");
     setActive(0);
-  }
-
-  React.useEffect(() => {
-    function watchScroll() {
-      window.addEventListener("scroll", logit);
-    }
-    watchScroll();
-    return () => {
-      window.removeEventListener("scroll", logit);
-    };
-  });
-
+  };
   return (
-    <div className="accordian">
-      <div className="accordian__heading">
-        <div className="container" onClick={handleActive}>
-          <h3>{question}</h3>
-          {active === id ? <SlArrowUp size={25} /> : <SlArrowDown size={25} />}
+    <OnClickOutside onClickOutside={closeAllAccordion}>
+      <div className="accordian">
+        <div className="accordian__heading">
+          <div className="container" onClick={handleActive}>
+            <h3>{question}</h3>
+            {active === id ? (
+              <SlArrowUp size={25} />
+            ) : (
+              <SlArrowDown size={25} />
+            )}
+          </div>
+        </div>
+        <div className={active === id ? "show" : "accordian__content"}>
+          <div className="container">
+            <p>{answer}</p>
+          </div>
         </div>
       </div>
-      <div className={active === id ? "show" : "accordian__content"}>
-        <div className="container">
-          <p>{answer}</p>
-        </div>
-      </div>
-    </div>
+    </OnClickOutside>
   );
 }
